6955d11d2d2d01408738209bd07614a936dbd529
nathanielgrossman <nategrossman@mac.com>
2019-12-21 14:20:33 -0800

    setup locktask mode

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+// @ts-ignore
+import RNLockTask from 'react-native-lock-task'
+
 import Main from './javascript/Main'
 
 const App = () => {
+  useEffect(() => RNLockTask.startLockTask(), [])
   return <Main />
 }
 
