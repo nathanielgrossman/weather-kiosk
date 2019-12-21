9bc2ece626197c5db11d58408ba8a03207f99a94
nathanielgrossman <nategrossman@mac.com>
2019-12-20 21:21:19 -0800

    rain module

--- javascript/components/RainModule.tsx
+++ javascript/components/RainModule.tsx
@@ -1,21 +1,40 @@
-import React, { useContext } from 'react'
+import React, { useContext, useMemo } from 'react'
 import { StyleSheet, Text } from 'react-native'
 
 import DashboardModule from './DashboardModule'
+import UIText from './ui/UIText'
 
 import StationContext from '../contexts/StationContext'
+import { mmToIn } from '../utils/conversions'
 
 import COLORS from '../constants/colors'
 
 const RainModule = () => {
   const { rainDashboardData } = useContext(StationContext)
+  const { sum_rain_1, sum_rain_24 } = rainDashboardData || {}
+
+  const sum1 = useMemo(() => mmToIn(sum_rain_1), [sum_rain_1])
+
+  const sum24 = useMemo(() => mmToIn(sum_rain_24), [sum_rain_24])
+
   return (
     <DashboardModule color={COLORS.blue3}>
       <>
-        <Text style={{ color: COLORS.tan }}>RainModule</Text>
-        <Text style={{ color: COLORS.tan }}>
-          {JSON.stringify(rainDashboardData, null, 2)}
-        </Text>
+        <UIText color="tan" size="medium" font="plexLight">
+          Rain
+        </UIText>
+        <UIText color="tan" size="xl" font="eczar" style={STYLES.mainReadout}>
+          {sum1}"
+        </UIText>
+        <UIText color="tan" size="small" font="plexBold">
+          Past 1h
+        </UIText>
+        <UIText color="tan" size="xl" font="eczar" style={STYLES.mainReadout}>
+          {sum_rain_24}"
+        </UIText>
+        <UIText color="tan" size="small" font="plexBold">
+          Past 24h
+        </UIText>
       </>
     </DashboardModule>
   )
@@ -24,6 +43,11 @@ const RainModule = () => {
 const STYLES = StyleSheet.create({
   flex: {
     flex: 1,
+  },
+  mainReadout: {
+    marginBottom: -80,
+    marginTop: -50,
+    lineHeight: 172,
   },
 })
 
--- javascript/utils/conversions.ts
+++ javascript/utils/conversions.ts
@@ -2,3 +2,8 @@ export const cToF = (celsius?: number) => {
   if (!celsius) return -1
   return ((celsius * 9) / 5 + 32).toFixed(1)
 }
+
+export const mmToIn = (mm?: number) => {
+  if (!mm) return 0.00
+  return (mm * 0.0393701).toFixed(2)
+}
